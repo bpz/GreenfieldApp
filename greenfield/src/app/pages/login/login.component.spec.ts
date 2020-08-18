52b2e9a9b6dcc8cc1cc8c34b02c7ae9e1cc88e5c
Beatriz Hernandez <nammy.he@gmail.com>
2020-08-18 15:29:28 +0200

    Add test for cleaning form in login
    After adding the data, and login, it is cleaned.

--- greenfield/src/app/pages/login/login.component.spec.ts
+++ greenfield/src/app/pages/login/login.component.spec.ts
@@ -4,16 +4,25 @@ import { AuthService } from 'src/app/services/auth.service';
 import { HttpClientModule } from '@angular/common/http';
 import { FormBuilder } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  class MockAuthService {
+    isLoggedIn = of(true);
+    token = "1234";
+    login = function (user: string, pass: string) { }
+  }
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientModule, ReactiveFormsModule],
       declarations: [LoginComponent],
-      providers: [AuthService, FormBuilder]
+      providers: [
+        FormBuilder,
+        { provide: AuthService, useClass: MockAuthService }]
     })
       .compileComponents();
   }));
@@ -44,16 +53,32 @@ describe('LoginComponent', () => {
 
   it('should have a password input', () => {
     fixture.detectChanges();
-    const usernameElement: HTMLElement = fixture.nativeElement;
-    const input = usernameElement.querySelector('.container .loginBox form .loginForm #ipassword');
+    const passwordElement: HTMLElement = fixture.nativeElement;
+    const input = passwordElement.querySelector('.container .loginBox form .loginForm #ipassword');
     expect(input.attributes.getNamedItem('type').textContent).toEqual('password');
     expect(input.attributes.getNamedItem('placeholder').textContent).toEqual('Password');
   });
 
   it('login bottom says Login', () => {
     fixture.detectChanges();
-    const usernameElement: HTMLElement = fixture.nativeElement;
-    const btn = usernameElement.querySelector('.container .loginBox form button');
+    const buttonElement: HTMLElement = fixture.nativeElement;
+    const btn = buttonElement.querySelector('.container .loginBox form button');
     expect(btn.textContent).toEqual('Login');
+  });
+
+  it('login cleans form', () => {
+    fixture.detectChanges();
+
+    const user = "1234";
+    const pass = "1234";
+    component.onSubmit({ user: user, password: pass });
+
+    const passwordElement: HTMLElement = fixture.nativeElement;
+    const passInput = passwordElement.querySelector('.container .loginBox form .loginForm #ipassword');
+    expect(passInput.textContent).toEqual("");
+
+    const usernameElement: HTMLElement = fixture.nativeElement;
+    const userInput = usernameElement.querySelector('.container .loginBox form .loginForm #iusername');
+    expect(userInput.textContent).toEqual("");
   });
 });
